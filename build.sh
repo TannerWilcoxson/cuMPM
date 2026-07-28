6b6a503c1cd7b639c629911652d6007f6d4a069d
Tanner Wilcoxson <tannerwilcoxson@gmail.com>
2026-07-28 16:19:06 -0500

    Untrack local build scripts and legacy tests

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export PATH=$(echo $PATH | tr ':' '\n' | grep -v '(' | tr '\n' ':')
-export PATH=/usr/local/cuda/bin:$PATH
-cd /home/tanner/cuMPM
-/home/tanner/micromamba/envs/cuMPM/bin/pip install -e . --no-build-isolation
